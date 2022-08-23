87b03fb815d1a4d9f4c9b28c65340c02c5f85c3f
codelikecraze <63984309+CodeLikeCrazE@users.noreply.github.com>
2022-08-23 13:55:07 -0700

    fix

--- main.js
+++ main.js
@@ -11,76 +11,74 @@
 //  powered by bad dumb code
 
 
-window.onload = () => {
-    console.log("pricons is running on this page!");
-    setInterval(tick, 0);
-    window.pricons = {
-        version: "v1.0",
-        api: [
-            {
-                changeUpdateRate: function(rate) {
-                    window.pricons.internal.updateRate = rate;
-                },
-                setEnabled: function(enabled) {
-                    window.pricons.internal.enabled = enabled;
-                }
+console.log("pricons is running on this page!");
+setInterval(tick, 0);
+window.pricons = {
+    version: "v1.0",
+    api: [
+        {
+            changeUpdateRate: function (rate) {
+                window.pricons.internal.updateRate = rate;
+            },
+            setEnabled: function (enabled) {
+                window.pricons.internal.enabled = enabled;
             }
-        ],
-        internal: {
-            updateCounter:0,
-            updateRate:0,
-            enabled:true,
-            icons: {
-                enby: "https://www.prideflags.org/static/search/data/img/nonbinary_pride_flag.svg",
-                progress: "https://www.prideflags.org/static/search/data/img/progress_pride_flag.svg",
-                agender: "https://www.prideflags.org/static/search/data/img/agender_pride_flag.svg",
-                agender9: "https://www.prideflags.org/static/search/data/img/agender_pride_flag_9_stripe.svg",
-                agenderDotInfo: "https://www.prideflags.org/static/search/data/img/agender_pride_flag_agender_info.svg",
-                aromantic: "https://www.prideflags.org/static/search/data/img/aromantic_pride_flag.svg",
-                ase: "https://www.prideflags.org/static/search/data/img/asexual_pride_flag.svg",
-                bear: "https://www.prideflags.org/static/search/data/img/bear_pride_flag.svg",
-                bigender: "https://www.prideflags.org/static/search/data/img/bigender_pride_flag.svg",
-                bigender7: "https://www.prideflags.org/static/search/data/img/bigender_pride_flag_7_stripe.svg",
-                bi: "https://www.prideflags.org/static/search/data/img/bisexual_pride_flag.svg",
-                bxy: "https://www.prideflags.org/static/search/data/img/bxy_pride_flag.svg",
-                demiboy: "https://www.prideflags.org/static/search/data/img/demiboy_pride_flag.svg",
-                demigirl: "https://www.prideflags.org/static/search/data/img/demigirl_pride_flag.svg",
-                demiromantic: "https://www.prideflags.org/static/search/data/img/demiromantic_pride_flag.svg",
-                demisexual: "https://www.prideflags.org/static/search/data/img/demisexual_pride_flag.svg",
-                doe: "https://www.prideflags.org/static/search/data/img/doe_pride_flag.svg",
-                gay: "https://www.prideflags.org/static/search/data/img/gay_pride_1979.svg",
-                phili: "https://www.prideflags.org/static/search/data/img/gay_pride_flag_philadelphia.svg",
-                gayuk: "https://www.prideflags.org/static/search/data/img/gay_pride_flag_uk.svg",
-                gendercreative: "https://www.prideflags.org/static/search/data/img/gendercreative_pride_flag.svg",
-                genderfluid: "https://www.prideflags.org/static/search/data/img/genderfluid_pride_flag.svg",
-                genderflux: "https://www.prideflags.org/static/search/data/img/genderflux_pride_flag.svg",
-                genderqueer: "https://www.prideflags.org/static/search/data/img/genderqueer_pride_flag.svg",
-                gxrl: "https://www.prideflags.org/static/search/data/img/gxrl_pride_flag.svg",
-                intersex: "https://www.prideflags.org/static/search/data/img/intersex_pride_flag.svg",
-                natalie: "https://www.prideflags.org/static/search/data/img/bigender_pride_flag.svg",
-                lesbianfem: "https://www.prideflags.org/static/search/data/img/lesbian_pride_flag_labrys.svg",
-                lesbianpride: "https://www.prideflags.org/static/search/data/img/lesbian_pride_flag_rainbow.svg",
-                lesbiankiss: "https://www.prideflags.org/static/search/data/img/lipstick_lesbian_pride_flag_with_kiss.svg",
-                lesbian: "https://www.prideflags.org/static/search/data/img/lipstick_lesbian_pride_flag_without_kiss.svg",
-                littlefluid: "https://www.prideflags.org/static/search/data/img/littlefluid_pride_flag.svg",
-                neutrois: "https://www.prideflags.org/static/search/data/img/neutrois_pride_flag.svg",
-                enbypride: "https://www.prideflags.org/static/search/data/img/nonbinary_pride_flag_transrants.svg",
-                nxnbinary: "https://www.prideflags.org/static/search/data/img/nxnbinary_pride_flag.svg",
-                nxnbinarygreen: "https://www.prideflags.org/static/search/data/img/nxnbinary_pride_flag_green.svg",
-                nxnbinarypride: "https://www.prideflags.org/static/search/data/img/nxnbinary_pride_flag_purple.svg",
-                pangenderwhite: "https://www.prideflags.org/static/search/data/img/pangender_pride_flag_white_center.svg",
-                pangenderyellow: "https://www.prideflags.org/static/search/data/img/pangender_pride_flag_yellow_center.svg",
-                pan: "https://www.prideflags.org/static/search/data/img/pansexual_pride_flag.svg",
-                pocket: "https://www.prideflags.org/static/search/data/img/pocket_gender_pride_flag.svg",
-                pocketpride: "https://www.prideflags.org/static/search/data/img/pocket_gender_pride_flag_sleepygender.svg",
-                poly: "https://www.prideflags.org/static/search/data/img/polysexual_pride_flag.svg",
-                transby: "https://www.prideflags.org/static/search/data/img/new_trans_nonbinary_bbipoc_flag.svg",
-                intertrans: "https://www.prideflags.org/static/search/data/img/trans_intersex_pride_flag.svg",
-                newprideflag: "https://www.prideflags.org/static/search/data/img/new_queer_trans_pride_flag.svg",
-                trans: "https://www.prideflags.org/static/search/data/img/transgender_pride_flag.svg",
-                isrealtrans: "https://www.prideflags.org/static/search/data/img/transgender_pride_flag_israel.svg",
-                transpride: "https://www.prideflags.org/static/search/data/img/transgender_pride_flag_jennifer.svg",
-            }
+        }
+    ],
+    internal: {
+        updateCounter: 0,
+        updateRate: 0,
+        enabled: true,
+        icons: {
+            enby: "https://www.prideflags.org/static/search/data/img/nonbinary_pride_flag.svg",
+            progress: "https://www.prideflags.org/static/search/data/img/progress_pride_flag.svg",
+            agender: "https://www.prideflags.org/static/search/data/img/agender_pride_flag.svg",
+            agender9: "https://www.prideflags.org/static/search/data/img/agender_pride_flag_9_stripe.svg",
+            agenderDotInfo: "https://www.prideflags.org/static/search/data/img/agender_pride_flag_agender_info.svg",
+            aromantic: "https://www.prideflags.org/static/search/data/img/aromantic_pride_flag.svg",
+            ase: "https://www.prideflags.org/static/search/data/img/asexual_pride_flag.svg",
+            bear: "https://www.prideflags.org/static/search/data/img/bear_pride_flag.svg",
+            bigender: "https://www.prideflags.org/static/search/data/img/bigender_pride_flag.svg",
+            bigender7: "https://www.prideflags.org/static/search/data/img/bigender_pride_flag_7_stripe.svg",
+            bi: "https://www.prideflags.org/static/search/data/img/bisexual_pride_flag.svg",
+            bxy: "https://www.prideflags.org/static/search/data/img/bxy_pride_flag.svg",
+            demiboy: "https://www.prideflags.org/static/search/data/img/demiboy_pride_flag.svg",
+            demigirl: "https://www.prideflags.org/static/search/data/img/demigirl_pride_flag.svg",
+            demiromantic: "https://www.prideflags.org/static/search/data/img/demiromantic_pride_flag.svg",
+            demisexual: "https://www.prideflags.org/static/search/data/img/demisexual_pride_flag.svg",
+            doe: "https://www.prideflags.org/static/search/data/img/doe_pride_flag.svg",
+            gay: "https://www.prideflags.org/static/search/data/img/gay_pride_1979.svg",
+            phili: "https://www.prideflags.org/static/search/data/img/gay_pride_flag_philadelphia.svg",
+            gayuk: "https://www.prideflags.org/static/search/data/img/gay_pride_flag_uk.svg",
+            gendercreative: "https://www.prideflags.org/static/search/data/img/gendercreative_pride_flag.svg",
+            genderfluid: "https://www.prideflags.org/static/search/data/img/genderfluid_pride_flag.svg",
+            genderflux: "https://www.prideflags.org/static/search/data/img/genderflux_pride_flag.svg",
+            genderqueer: "https://www.prideflags.org/static/search/data/img/genderqueer_pride_flag.svg",
+            gxrl: "https://www.prideflags.org/static/search/data/img/gxrl_pride_flag.svg",
+            intersex: "https://www.prideflags.org/static/search/data/img/intersex_pride_flag.svg",
+            natalie: "https://www.prideflags.org/static/search/data/img/bigender_pride_flag.svg",
+            lesbianfem: "https://www.prideflags.org/static/search/data/img/lesbian_pride_flag_labrys.svg",
+            lesbianpride: "https://www.prideflags.org/static/search/data/img/lesbian_pride_flag_rainbow.svg",
+            lesbiankiss: "https://www.prideflags.org/static/search/data/img/lipstick_lesbian_pride_flag_with_kiss.svg",
+            lesbian: "https://www.prideflags.org/static/search/data/img/lipstick_lesbian_pride_flag_without_kiss.svg",
+            littlefluid: "https://www.prideflags.org/static/search/data/img/littlefluid_pride_flag.svg",
+            neutrois: "https://www.prideflags.org/static/search/data/img/neutrois_pride_flag.svg",
+            enbypride: "https://www.prideflags.org/static/search/data/img/nonbinary_pride_flag_transrants.svg",
+            nxnbinary: "https://www.prideflags.org/static/search/data/img/nxnbinary_pride_flag.svg",
+            nxnbinarygreen: "https://www.prideflags.org/static/search/data/img/nxnbinary_pride_flag_green.svg",
+            nxnbinarypride: "https://www.prideflags.org/static/search/data/img/nxnbinary_pride_flag_purple.svg",
+            pangenderwhite: "https://www.prideflags.org/static/search/data/img/pangender_pride_flag_white_center.svg",
+            pangenderyellow: "https://www.prideflags.org/static/search/data/img/pangender_pride_flag_yellow_center.svg",
+            pan: "https://www.prideflags.org/static/search/data/img/pansexual_pride_flag.svg",
+            pocket: "https://www.prideflags.org/static/search/data/img/pocket_gender_pride_flag.svg",
+            pocketpride: "https://www.prideflags.org/static/search/data/img/pocket_gender_pride_flag_sleepygender.svg",
+            poly: "https://www.prideflags.org/static/search/data/img/polysexual_pride_flag.svg",
+            transby: "https://www.prideflags.org/static/search/data/img/new_trans_nonbinary_bbipoc_flag.svg",
+            intertrans: "https://www.prideflags.org/static/search/data/img/trans_intersex_pride_flag.svg",
+            newprideflag: "https://www.prideflags.org/static/search/data/img/new_queer_trans_pride_flag.svg",
+            trans: "https://www.prideflags.org/static/search/data/img/transgender_pride_flag.svg",
+            isrealtrans: "https://www.prideflags.org/static/search/data/img/transgender_pride_flag_israel.svg",
+            transpride: "https://www.prideflags.org/static/search/data/img/transgender_pride_flag_jennifer.svg",
         }
     }
 }
